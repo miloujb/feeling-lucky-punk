e45b8b5d26bb38634fcf7e50a668bc6055b07973
miloujb <emilyjbailey@hotmail.co.uk>
2023-02-04 23:00:05 +0000

    Updates

---
+++ precache-manifest.b58a59c73ddb01ba1ce388b1fff0037d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd6048c8fedfc1ea771344aa016d7e3",
+    "url": "/feeling-lucky-punk/index.html"
+  },
+  {
+    "revision": "cdbe0b63ef76ac18e2e6",
+    "url": "/feeling-lucky-punk/static/css/main.05af7174.chunk.css"
+  },
+  {
+    "revision": "c90b5101735f4d8d343f",
+    "url": "/feeling-lucky-punk/static/js/2.3ca3619e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/feeling-lucky-punk/static/js/2.3ca3619e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdbe0b63ef76ac18e2e6",
+    "url": "/feeling-lucky-punk/static/js/main.1cf04f61.chunk.js"
+  },
+  {
+    "revision": "daf9599c20ec0b57bbb7",
+    "url": "/feeling-lucky-punk/static/js/runtime-main.56decd96.js"
+  }
+]);
